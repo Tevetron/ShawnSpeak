75df2fad8891b2483f2a9fdd7c8b4a94bfebd1ee
Tevetron <friedrich.jonathan4@gmail.com>
2022-01-04 02:11:14 -0500

    Update messageCreate.js

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -1,8 +1,8 @@
 module.exports = (client, Discord, messageCreate) => {
     const config = require('../../config.json');
-    if(messageCreate.member.user.tag !== 'config.shawnid' 
-    && messageCreate.member.user.tag !== 'config.peterid'
-    && messageCreate.member.user.tag !== 'config.jonid') return;
+    if(messageCreate.member.user.tag !== config.shawnid
+    && messageCreate.member.user.tag !== config.peterid
+    && messageCreate.member.user.tag !== config.jonid) return;
     const args = messageCreate.content;
 
     const cmd = 'shawnspoke';
